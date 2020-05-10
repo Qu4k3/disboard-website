75764c7324faf0e14d246db463d672ecae60ac3b
Qu4k3 <paul.guillamon@gmail.com>
2020-05-10 19:09:26 +0200

    app

--- src/App.js
+++ src/App.js
@@ -10,24 +10,28 @@ import { Player } from './pages/Player';
 import { NotFound } from './pages/NotFound';
 import { BackToTop } from './components/BackToTop';
 
-import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+
+import 'normalize.css';
+import './styles/main.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header />
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/about/" component={About}/>
-          <Route exact path="/wars/" component={Wars}/>
-          <Route path="/war/:warId" component={War}/>
-          <Route exact path="/players/" component={Players}/>
-          <Route path="/player/:playerId" component={Player}/>
-          <Route component={NotFound} />
-        </Switch>
+        <main id="main" class="main">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about/" component={About}/>
+            <Route exact path="/wars/" component={Wars}/>
+            <Route path="/war/:warId" component={War}/>
+            <Route exact path="/players/" component={Players}/>
+            <Route path="/player/:playerId" component={Player}/>
+            <Route component={NotFound} />
+          </Switch>
+        </main>
         <Footer />
         <BackToTop/>
       </div>
